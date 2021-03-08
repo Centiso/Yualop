1339983d3b3c6f650c57b4a147a62757d7acc7c7
CECILE <csaivet72@gmail.com>
2021-03-08 12:01:58 +0100

    ajout 2

--- persp.c
+++ persp.c
@@ -19,27 +19,39 @@
  } t_pers ;
 
 
-int *crea_pers (int faction, int level){
-  t_pers *player;
+t_pers *crea_pers (int faction, int level){
+    t_pers *player;
 
-player=malloc(sizeof(t_pers)*1);
+  player=malloc(sizeof(t_pers)*1);
 
-if(faction==0){ // Est un gentil
-  printf("Saisir votre pseudo : ");
-  scanf("%s",&(player->nom)); printf("\n");
-  player->pdv = 100;
-  player->def = 50;
-  player->att = 5;
-  printf("Vous avez choisi le pseudo '%s' \n",player.nom);
-  printf("Vous avez %d points de vie ", player.pdv);
-  printf("%d points de defense (petit cadeau de depart) ", player.def);
-  printf("et %d de force de frappe, il faut bien demarrer quelque part !\n \n", player.att);
-}
+  if(faction==0){ // Est un gentil
+    printf("Saisir votre pseudo : ");
+    scanf("%s",&(player->nom)); printf("\n");
+    player->pdv = 100;
+    player->def = 50;
+    player->att = 5;
+    printf("Vous avez choisi le pseudo '%s' \n",player->nom);
+    printf("Vous avez %d points de vie ", player->pdv);
+    printf("%d points de defense (petit cadeau de depart) ", player->def);
+    printf("et %d de force de frappe, il faut bien demarrer quelque part !\n \n", player->att);
+  }
 
-if (faction ==1){ // Est un méchant
-  player->pdv = 100;
-  player->def = 25;
-  player->att = 3;
+  if (faction ==1){ // Est un méchant
+    player->pdv = 100;
+    player->def = 25;
+    player->att = 3;
+  }
+
+/*
+  printf(" %s 🧙 ", player.nom);
+  for (int coeur=0; coeur < ((player.pdv)/2); coeur++ ) {printf("🟦");}
+  printf("\n"); printf(" %d🛡️ ", player.def); printf(" %d🗡️ \n", player.att);
+*/
+printf(" %s <(^-^)> ", player->nom);
+for (int coeur=0; coeur < ((player->pdv)/2); coeur++ ) {printf("#");}
+printf("\n"); printf(" %d [D]", player->def); printf(" %d o==[]::::::::> \n", player->att);
+
+return (player);
 }
 
 void maj_lvl(int level, t_pers *player){
@@ -47,30 +59,18 @@ void maj_lvl(int level, t_pers *player){
   if (faction ==0){
     for (int i = 0;i<level;i++){ //gentils
 
-        pdv = pdv *0.1;
-        def  = def * 0.1;
-        att = att * 0.1;
+        player->pdv = pdv *0.1;
+        player->def  = def * 0.1;
+        player->att = att * 0.1;
     }
   }
   if (faction ==1){
     for (int i = 0;i<level;i++){ //mechants
-        pdv = pdv *0.1;
-        def  = def * 0.1;
-        att = att * 0.1;
+        player->pdv = pdv *0.1;
+        player->def  = def * 0.1;
+        player->att = att * 0.1;
     }
   }
-}
-
-/*
-  printf(" %s 🧙 ", player.nom);
-  for (int coeur=0; coeur < ((player.pdv)/2); coeur++ ) {printf("🟦");}
-  printf("\n"); printf(" %d🛡️ ", player.def); printf(" %d🗡️ \n", player.att);
-*/
-printf(" %s <(^-^)> ", player.nom);
-for (int coeur=0; coeur < ((player.pdv)/2); coeur++ ) {printf("#");}
-printf("\n"); printf(" %d [D]", player.def); printf(" %d o==[]::::::::> \n", player.att);
-
-return (player);
 }
 
 int main (){
