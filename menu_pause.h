cf15c65649be8255f0079f398096794120de69d3
xMa97 <45797670+xMa97@users.noreply.github.com>
2021-04-19 23:33:45 +0200

    Delete menu_pause.h

--- menu_pause.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MENU_PAUSE_H_
-#define _MENU_PAUSE_H_
-
-void in_game_options(SDL_Window *window, SDL_Renderer *renderer);
-
-SDL_bool menu_pause(SDL_Window *window, SDL_Renderer *renderer, t_pers *player);
-
-#endif
